92767508235fd69f22021e653370996df795378f
Majid Ansari <33059093+Majid023@users.noreply.github.com>
2018-09-14 21:28:45 +0530

    add two number

---
+++ sum of tow number.cpp
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<iostream>
+
+using namespace std;
+
+int main( ) {
+	
+	int a = 0, b = 0;
+	
+	cout << "Enter two numbers \n";
+	cin >> a >> b;
+	
+	int sum = a + b;
+	
+	cout << "sum of a and b is " << sum;
+	
+	getch();
+	return 0;
+}
